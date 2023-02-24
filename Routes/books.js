79a7056114617eeb005ead9d3207e5f70b31c4d6
kunalbarve02 <kunalbarve02@gmail.com>
2023-02-24 17:06:26 +0530

    added getAllBooksSortedByRating

--- Routes/books.js
+++ Routes/books.js
@@ -1,12 +1,11 @@
 const express = require("express");
-const { getAllBooks,getBookById,getAllBooksSortedByPrice,getFilteredBooks } = require("../controllers/books");
+const { getAllBooks,getBookById,getAllBooksSortedByPrice,getFilteredBooks,getAllBooksSortedByRating } = require("../controllers/books");
 const router = express.Router();
 
 router.get("/books", getAllBooks)
 router.get("/books/:bookId",getBookById)
 router.get("/books/sort/price",getAllBooksSortedByPrice)
 router.get("/books/sort/rating",getAllBooksSortedByRating)
-router.get("/books/sort/price",getAllBooksSortedByPrice)
 router.get("/books/filter",getFilteredBooks)
 
 
